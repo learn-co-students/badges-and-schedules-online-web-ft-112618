1ffc343019d470993726ee71cdae8832b6edd891
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-29 23:04:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch batch_badge_creator(attendees)
+  arr = []
+  attendees.each do |name|
+    arr << badge_maker(name)
+  end
+  return arr
+
+end
